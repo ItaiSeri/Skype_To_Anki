8407a80b181843d1747caa5155c65aade5b929a4
unknown <itai7se@gmail.com>
2020-07-14 20:43:27 +0300

    got Chinese characters right

--- SkypeToCSV.py
+++ SkypeToCSV.py
@@ -1,3 +1,7 @@
+# coding: utf8
+
+#import os, sys
+
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -9,19 +13,30 @@ from googletrans import Translator
 path=Path("F:\Dowloads\8_itai.seri_export/messages.json")
 expath=Path("F:/Dowloads")
 
-msg =pd.read_json(path) #output: DataFrame, all relavent data in 'conversations' column 
+msg =pd.read_json(path,encoding='utf-8') #output: DataFrame, all relavent data in 'conversations' column 
 conver=msg.conversations #output: Series
 norm=json_normalize(conver) #output: DataFrame, breaks up Series to DataFrame (with meaningfull columns)
 ########
 an=norm.MessageList[norm.displayName=='Anne Wu']
 an_df=pd.DataFrame(an[9])
-#an_df.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
+an_df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
 ############
 
 multiple_df=norm.MessageList.apply(lambda x: pd.DataFrame(x))
 df=pd.concat(multiple_df.tolist())
-df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
+#df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
 
 translator = Translator()
-print (translator.translate('小伙子怎么了',src='zh-CN').text)
-print (translator.translate('hello world').src)
+#print (translator.translate('小伙子怎么了',src='zh-CN').text)
+#print (translator.translate('hello world').src)
+
+
+
+an_df['tr']=an_df.content.apply(lambda x: translator.translate(x).text)
+
+#an_df['tr']=an_df.content.apply(lambda x: len(x))
+
+
+
+
+
