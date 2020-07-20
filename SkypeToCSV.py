2c0de3febdca8240c2f5ae4d450f1d33e984e901
unknown <itai7se@gmail.com>
2020-07-20 06:47:21 +0300

    fasttext testing

--- SkypeToCSV.py
+++ SkypeToCSV.py
@@ -9,6 +9,11 @@ from pandas.io.json import json_normalize
 
 import googletrans
 from googletrans import Translator
+translator = Translator()
+import cld2
+
+
+src_lang='zh-CN'
 
 path=Path("F:\Dowloads\8_itai.seri_export/messages.json")
 expath=Path("F:/Dowloads")
@@ -21,21 +26,35 @@ an=norm.MessageList[norm.displayName=='Anne Wu']
 an_df=pd.DataFrame(an[9])
 an_df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
 an_df['content_trimmed']= an_df['content'].str.replace(" ","")
-an_df['tr']=an_df.content_trimmed.apply(lambda x: translator.translate(x).text)
+#an_df['is_src_lang']= an_df['content'].apply(lambda x: [translator.detect(word).lang==src_lang for word in x.split()])
+#an_df['tr']=an_df.content_trimmed.apply(lambda x: translator.translate(x).text)
 ############
 
 multiple_df=norm.MessageList.apply(lambda x: pd.DataFrame(x))
 df=pd.concat(multiple_df.tolist())
 #df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
 
-translator = Translator()
-#print (translator.translate('小伙子怎么了',src='zh-CN').text)
-#print (translator.translate('hello world').src)
+#isReliable, textBytesFound, details = cld2.detect("王明：那是杂志吗")
+#print('  reliable: %s' % (isReliable != 0))
+#print('  textBytes: %s' % textBytesFound)
+#print('  details: %s' % str(details))
+
+from pycountry import languages
+import fasttext
+fasttext_model_path=r"C:\Users\Itai\Anaconda3\Lib\site-packages\fasttext/lid.176.bin"
+model = fasttext.load_model(fasttext_model_path)
+
+sentences = ['需']
+predictions = model.predict(sentences,k=5)[0][0]
+for pred in predictions:
+    print (languages.get(alpha_2=pred.replace('__label__','')).name)
+#print(predictions)
+
+lang_name = languages.get(alpha_2=model.predict(sentences)[0][0][0].replace('__label__','')).name
+#print(lang_name)
 
 
 
-
-#an_df['tr']=an_df.content.apply(lambda x: len(x))
 
 
 
