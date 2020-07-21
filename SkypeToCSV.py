1b6acaca98e02e4b3eb6c9d44d558fcaf4ad0493
unknown <itai7se@gmail.com>
2020-07-21 17:01:49 +0300

    fixed list bug

--- SkypeToCSV.py
+++ SkypeToCSV.py
@@ -18,10 +18,10 @@ fasttext_model_path=r"C:\Users\Itai\Anaconda3\Lib\site-packages\fasttext/lid.176
 model = fasttext.load_model(fasttext_model_path)
 src_lang_='Chinese'
 
-sentences = ['zhè ér fù jìn yǒu chē zhàn ma ？ 这  儿 附 近  有  车  站   吗 ?']
+sentences = 'wǒ qù dà chéng shì de diàn yǐng yuàn kàn diàn yǐng 。 \n我 去 大 城    市  的 电   影   院   看  电   影   。 '
 def check_lang(character,src_lang_):
     lang_list=[]
-    predictions = model.predict(character,k=5)[0][0]
+    predictions = model.predict(character,k=5)[0]
     for pred in predictions:
         pred=pred.replace('__label__','')
         if languages.get(alpha_2=pred) is not None:
@@ -41,7 +41,7 @@ norm=json_normalize(conver) #output: DataFrame, breaks up Series to DataFrame (w
 an=norm.MessageList[norm.displayName=='Anne Wu']
 an_df=pd.DataFrame(an[9])
 an_df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
-an_df['content_trimmed']= an_df['content'].str.replace(" ","")
+an_df['content_trim_split']= an_df['content'].str.replace(" ","").splitlines()
 #an_df['is_src_lang']= an_df['content'].apply(lambda x: [translator.detect(word).lang==src_lang for word in x.split()])
 an_df['is_src_lang']= an_df['content'].apply(lambda x: [check_lang(word,src_lang_) for word in x.split()])
 #an_df['tr']=an_df.content_trimmed.apply(lambda x: translator.translate(x).text)
@@ -52,13 +52,16 @@ df=pd.concat(multiple_df.tolist())
 #df.content.to_csv(expath/'conv.txt', header=None, index=None, mode='a')
 
 
+for x in b:
+    print (check_lang(x,src_lang_))
 
-  
+ #check_lang(x,src_lang_ 
 #print(predictions)
 
 #if languages.get(alpha_2='he') is not None:
 #    lang_name = languages.get(alpha_2='he').name
 #print(lang_name)
+
 
 
 
