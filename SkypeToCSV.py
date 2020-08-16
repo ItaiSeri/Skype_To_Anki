509db3c31b751856e89776514f4d7abc3467d98f
unknown <itai7se@gmail.com>
2020-08-16 06:45:19 +0300

    Turned it into a working function

--- SkypeToCSV.py
+++ SkypeToCSV.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import fasttext
 from pathlib import Path
-from pandas.io.json import json_normalize
 from itertools import compress
 from googletrans import Translator
 from pycountry import languages
@@ -14,55 +13,61 @@ translator = Translator() #googletrans
 fasttext_model_path=r"C:\Users\Itai\Anaconda3\Lib\site-packages\fasttext\lid.176.bin" 
 fasttext_model = fasttext.load_model(fasttext_model_path)
 
-src_lang_='Chinese'
+src_lang='Chinese'
+src_langgoogle_code= 'zh-CN' #code list can be found here: https://cloud.google.com/translate/docs/languages
 
 
-def check_lang(character,src_lang_):
+def check_lang(character,src_lang):
     lang_list=[]
     predictions = fasttext_model.predict(character,k=5,threshold=0.7)[0]
     for pred in predictions:
         pred=pred.replace('__label__','')
         if languages.get(alpha_2=pred) is not None:
             lang_list.append(languages.get(alpha_2=pred).name)
-    return src_lang_ in lang_list  
+    return src_lang in lang_list  
 
 def SplitAndTrim(x): 
     return x.replace(" ","").splitlines()
-    
-src_lang='zh-CN'
 
-Skype_Json=Path(r"F:\Dowloads\8_itai.seri_export\messages.json")
+
+    
+#src_lang='zh-CN'
+
+Skype_file_path=Path(r"F:\Dowloads\8_itai.seri_export\messages.json")
 expath=Path("F:/Dowloads")
 contact_list=['Wendy K','Anne Wu']
-#def Skype2Anki(contact_list):
+
+def Skype2Anki(Skype_file_path,contact_list,src_lang):
     #add date slicing - try finding something more accurate than original arrival time
     #maby filter with messagetype
     #add .split('') to split by for instance =. like in Wendy K
     
-#Read conversations column from Skype's Json file, then normalize. I.E Break up Series to df with meaningful columns
-normalized=json_normalize(pd.read_json(Skype_Json,encoding='utf-8').conversations)
+    
+    #Read conversations column from Skype's Json file, then normalize. I.E Break up Series to df with meaningful columns
+    normalized=pd.json_normalize(pd.read_json(Skype_file_path,encoding='utf-8').conversations)
 
-#Convert the MessageList of every contact to df, then concat all of them into one df 
-df=pd.concat(normalized.MessageList[normalized.displayName.isin(contact_list)].apply(lambda x: pd.DataFrame(x)).tolist())
+    #Convert the MessageList of every contact to df, then concat all of them into one df 
+    df=pd.concat(normalized.MessageList[normalized.displayName.isin(contact_list)].apply(lambda x: pd.DataFrame(x)).tolist())
 
-#delete white spaces needs to be only in asian languages
-df['content_trim_split']= df['content'].apply(SplitAndTrim)
- #apply the function for every word
-df['is_src_lang']= df['content_trim_split'].apply(lambda x: [check_lang(string,src_lang_) for string in x])
-#Delete lists where text is not in the source language
-df['clean_content']= df[['content_trim_split','is_src_lang']].apply(tuple,axis=1).apply(lambda x: list(compress(x[0],x[1])))
+    #delete white spaces needs to be only in asian languages
+    df['content_trim_split']= df['content'].apply(SplitAndTrim)
+     #apply the function for every word
+    df['is_src_lang']= df['content_trim_split'].apply(lambda x: [check_lang(string,src_lang) for string in x])
+    #Delete lists where text is not in the source language
+    df['clean_content']= df[['content_trim_split','is_src_lang']].apply(tuple,axis=1).apply(lambda x: list(compress(x[0],x[1])))
 
-# Leave only rows with non empty list. "not x": True for empty list, False for not empty list
-# Split rows with multiple lists to multiple rows (explode)
-fltrd_df=df.clean_content[df['clean_content'].apply(lambda x: not x) ==False].explode().reset_index(drop=True) 
+    # Leave only rows with non empty list. "not x": True for empty list, False for not empty list
+    # Split rows with multiple lists to multiple rows (explode)
+    fltrd_df=df.clean_content[df['clean_content'].apply(lambda x: not x) ==False].explode().reset_index(drop=True) 
 
-#Transle every row to destination language
-Translation=fltrd_df.apply(lambda x: translator.translate(x).text)
-#Get pronunciation of source language for every row
-Pinyin=fltrd_df.apply(lambda x: translator.translate(x,dest='zh-CN').pronunciation)
-Anki_CSV=pd.concat([fltrd_df,Translation,Pinyin],axis=1)
-Anki_CSV.columns=['Source','Translation','Pinyin']
-Anki_CSV.to_csv(expath/'Anki_CSV.txt', index=None, mode='a')
+    #Transle every row to destination language
+    Translation=fltrd_df.apply(lambda x: translator.translate(x).text)
+    #Get pronunciation of source language for every row
+    Pinyin=fltrd_df.apply(lambda x: translator.translate(x,dest=src_langgoogle_code).pronunciation)
+    Anki_CSV=pd.concat([fltrd_df,Translation,Pinyin],axis=1)
+    Anki_CSV.columns=['Source','Translation','Pinyin']
+    Anki_CSV.to_csv(expath/'Anki_CSV.txt', index=None, mode='a')
+    print('CSV file created!')
 
 
     
